78dc73b20921a7044ef03d1bb7488927649901d4
SPORTRADAR\r.fagundo <r.fagundo@sportradar.com>
2024-06-10 10:44:53 +0200

    Starting Pam's component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import VersionComponent from './components/VersionComponent.tsx';
 
-import baseball from './galery/baseball.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header" style={{ backgroundImage: `url(${baseball})` }} >
-        <p>
-          Website Versions By customer
-        </p>
        <VersionComponent/>
-      </header>
     </div>
   );
 }
