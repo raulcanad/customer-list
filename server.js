03a7eff4e20833f1abafaf072a426f8f9eda4470
SPORTRADAR\r.fagundo <r.fagundo@sportradar.com>
2024-07-03 16:46:29 +0200

    Queries out fo the server and serverDos

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { createPool, getConnection } = require('oracledb');
+const oracledb = require('oracledb');
 const cors = require('cors');
+const fs = require('fs/promises');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-// Enable CORS for all requests
+// Middleware
 app.use(cors());
 
 // Replace with your actual database credentials and connection details
@@ -17,129 +18,66 @@ const dbConfig = {
 
 // Function to initialize OracleDB connection pool
 async function initialize() {
-  try {
-    await createPool(dbConfig);
-    console.log('Connected to database');
-  } catch (err) {
-    console.error('Error connecting to database:', err.message);
-    process.exit(1); // Exit the process with failure code
-  }
-}
-
-// API endpoint to fetch DBCORE data
-app.get('/api/dbcore', async (req, res) => {
-  let connection;
-  try {
-    connection = await getConnection();
-    const result = await connection.execute('SELECT DISTINCT m.idmvversion FROM MMVERSION.MVCURRENTCOMPONENTVERSION m WHERE m.componentname = \'DBCORE\' AND m.tscreated = (SELECT MAX(mv.tscreated) FROM MMVERSION.MVCURRENTCOMPONENTVERSION mv WHERE mv.componentname = m.componentname)');
-    console.log('DBCORE Query Result:', result.rows);
-    res.send(result.rows);
-  } catch (err) {
-    console.error('Error fetching DBCORE data:', err.message);
-    res.status(500).send({ error: `Error fetching DBCORE data: ${err.message}` });
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
+    try {
+        await oracledb.createPool(dbConfig);
+        console.log('Connected to Oracle database');
       } catch (err) {
-        console.error('Error closing connection:', err.message);
+        console.error('Error connecting to database:', err.message);
+        process.exit(1); // Exit the process with failure code
+      }
+    }
+
+// Function to execute SQL query
+async function executeQuery(query, params = []) {
+    let connection;
+    try {
+      connection = await oracledb.getConnection();
+      const result = await connection.execute(query, params);
+      return result.rows;
+    } catch (err) {
+      throw err;
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error('Error closing connection:', err.message);
+        }
       }
     }
   }
-});
 
-// API endpoint to fetch MMFIXEDODDS data
-app.get('/api/mmfixedodds', async (req, res) => {
-  let connection;
-  try {
-    connection = await getConnection();
-    const result = await connection.execute('SELECT DISTINCT m.idmvversion FROM MMVERSION.MVCURRENTCOMPONENTVERSION m WHERE m.componentname = \'MMFIXEDODDS\' AND m.tscreated = (SELECT MAX(mv.tscreated) FROM MMVERSION.MVCURRENTCOMPONENTVERSION mv WHERE mv.componentname = m.componentname)');
-    console.log('MMFIXEDODDS Query Result:', result.rows);
-    res.send(result.rows);
-  } catch (err) {
-    console.error('Error fetching MMFIXEDODDS data:', err.message);
-    res.status(500).send({ error: `Error fetching MMFIXEDODDS data: ${err.message}` });
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error('Error closing connection:', err.message);
-      }
+// Read queries from JSON file
+async function readQueriesFromFile() {
+    try {
+      const data = await fs.readFile('queries.json', 'utf8');
+      return JSON.parse(data);
+    } catch (err) {
+      console.error('Error reading queries from file:', err.message);
+      throw err;
     }
   }
-});
-
-// API endpoint to fetch MMTEXAS data
-app.get('/api/mmtexas', async (req, res) => {
-    let connection;
-    try {
-      connection = await getConnection();
-      const result = await connection.execute('SELECT DISTINCT m.idmvversion FROM MMVERSION.MVCURRENTCOMPONENTVERSION m WHERE m.componentname = \'MMTEXAS\' AND m.tscreated = (SELECT MAX(mv.tscreated) FROM MMVERSION.MVCURRENTCOMPONENTVERSION mv WHERE mv.componentname = m.componentname)');
-      console.log('MMTEXAS Query Result:', result.rows);
-      res.send(result.rows);
-    } catch (err) {
-      console.error('Error fetching MMTEXAS data:', err.message);
-      res.status(500).send({ error: `Error fetching MMTEXAS data: ${err.message}` });
-    } finally {
-      if (connection) {
+// Start reading queries and setting up API endpoints
+readQueriesFromFile().then(queries => {
+    Object.entries(queries).forEach(([key, query]) => {
+      app.get(`/api/${key}`, async (req, res) => {
         try {
-          await connection.close();
+          const data = await executeQuery(query);
+          console.log(`${key.toUpperCase()} Query Result:`, data);
+          res.send(data);
         } catch (err) {
-          console.error('Error closing connection:', err.message);
+          console.error(`Error fetching ${key.toUpperCase()} data:`, err.message);
+          res.status(500).send({ error: `Error fetching ${key.toUpperCase()} data: ${err.message}` });
         }
-      }
-    }
+      });
+    });
+
+  // Start the server and initialize the database connection
+  app.listen(port, async () => {
+    console.log(`Server is running on port ${port}`);
+    await initialize();
   });
-
-  // API endpoint to fetch MMFOWARP data
-app.get('/api/mmfowarp', async (req, res) => {
-    let connection;
-    try {
-      connection = await getConnection();
-      const result = await connection.execute('SELECT DISTINCT m.idmvversion FROM MMVERSION.MVCURRENTCOMPONENTVERSION m WHERE m.componentname = \'MMFOWARP\' AND m.tscreated = (SELECT MAX(mv.tscreated) FROM MMVERSION.MVCURRENTCOMPONENTVERSION mv WHERE mv.componentname = m.componentname)');
-      console.log('MMFOWARP Query Result:', result.rows);
-      res.send(result.rows);
-    } catch (err) {
-      console.error('Error fetching MMFOWARP data:', err.message);
-      res.status(500).send({ error: `Error fetching MMFOWARP data: ${err.message}` });
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error('Error closing connection:', err.message);
-        }
-      }
-    }
-  });
-
-    // API endpoint to fetch MM data
-app.get('/api/mm', async (req, res) => {
-    let connection;
-    try {
-      connection = await getConnection();
-      const result = await connection.execute('SELECT DISTINCT m.idmvversion FROM MMVERSION.MVCURRENTCOMPONENTVERSION m WHERE m.componentname = \'MM\' AND m.tscreated = (SELECT MAX(mv.tscreated) FROM MMVERSION.MVCURRENTCOMPONENTVERSION mv WHERE mv.componentname = m.componentname)');
-      console.log('MM Query Result:', result.rows);
-      res.send(result.rows);
-    } catch (err) {
-      console.error('Error fetching MM data:', err.message);
-      res.status(500).send({ error: `Error fetching MM data: ${err.message}` });
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error('Error closing connection:', err.message);
-        }
-      }
-    }
-  });
-
-
-
-// Start the server and initialize the database connection
-app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
-  await initialize();
+}).catch(err => {
+  console.error('Failed to setup API endpoints:', err.message);
+  process.exit(1);
 });
