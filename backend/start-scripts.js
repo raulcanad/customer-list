26397a5561e8512742390690d9848a0d7214d35a
SPORTRADAR\r.fagundo <r.fagundo@sportradar.com>
2024-07-04 13:31:33 +0200

    Script to launch all servers at the same time

---
+++ backend/start-scripts.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+
+const scripts = [
+    'sporUat.js',
+    'sporProd.js',
+    'dazuUat.js',
+    'dazuProd.js',
+    'taloUat.js',
+    'taloProd.js'
+];
+
+scripts.forEach(script => {
+    const childProcess = spawn('node', [script]);
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(`${script} stdout: ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+        console.error(`${script} stderr: ${data}`);
+    });
+
+    childProcess.on('close', (code) => {
+        console.log(`${script} child process exited with code ${code}`);
+    });
+});
